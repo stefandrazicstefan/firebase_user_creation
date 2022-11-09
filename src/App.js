e5a6a9a58f79fceb428c1df8cbb5593aeb1b78ca
StefiThe Bog <stefandrazicstefan@gmail.com>
2022-11-09 18:06:59 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -90,6 +90,7 @@ function App() {
       </div>
       {isOpen && (
         <Modal
+          className="modal"
           setIsOpen={setIsOpen}
           user={currentUser}
           updateFunc={updateUsersInApp}
